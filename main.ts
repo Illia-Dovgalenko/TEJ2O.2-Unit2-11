d2f7b69507ea95076a99fa4b96c77308c60f6b7b
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-04-07 23:07:27 -0400

    Number Compare

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: Apr 2026
+ * This program Number Compare
 */
 
-basic.showString('Hello, World!')
+let number1 = 0
+let number2 = 0
+
+// generate two random numbers (0–99)
+basic.forever(function () {
+        number1 = randint(0, 99)
+        number2 = randint(0, 99)
+})
+
+// show first number
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(number1)
+})
+
+// show second number
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(number2)
+})
+
+// compare numbers
+input.onGesture(Gesture.Shake, function () {
+    if (number1 > number2) {
+        basic.showString("<")
+    } else {
+        basic.showString(">")
+    }
+})
