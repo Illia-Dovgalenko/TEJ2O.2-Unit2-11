9bdca141d17d8f5c412499936437836c36ca9130
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-04-14 13:02:06 -0400

    Number Compare

--- main.ts
+++ main.ts
@@ -5,8 +5,13 @@
  * This program Number Compare
 */
 
+// variables
 let number1 = 0
 let number2 = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
 // generate two random numbers (0–99)
 basic.forever(function () {
@@ -14,21 +19,21 @@ basic.forever(function () {
         number2 = randint(0, 99)
 })
 
-// show first number
+// show number1
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(number1)
 })
 
-// show second number
+// show number2
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(number2)
 })
 
 // compare numbers
 input.onGesture(Gesture.Shake, function () {
-    if (number1 > number2) {
+    if (number1 < number2) {
         basic.showString("<")
-    } else {
+    } else  {
         basic.showString(">")
     }
 })
